27007099593ae3e0720794dea032ced633bdc063
GustavoR <rocha.gustavo.1am@gmail.com>
2022-10-24 19:59:42 -0500

    Agregar archivos de proyecto.

---
+++ Back-endEjemplo/Models/OperasBas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Back_endEjemplo.Models
+{
+    public class OperasBas
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public int res { get; set; }
+
+        public  void Suma()
+        {
+            this.res = this.Num1 + this.Num2;
+        }
+
+        public void Resta()
+        {
+            this.res = this.Num1 - this.Num2;
+        }
+    }
+}
